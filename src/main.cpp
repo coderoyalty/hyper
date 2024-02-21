4465b09f8d98c29ba17ae3d4c94e3a5efbd63ec0
Akanni Emmanuel <akannie33@gmail.com>
2024-02-21 14:31:11 +0100

    draw an orange triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include "shader.hpp"
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -30,14 +31,91 @@ int main(int argc, char **argv)
 	}
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	// triangle data
+	const float vertices[] =
+			{
+					-0.5f,
+					-0.5f,
+					0.0f,
+					+0.5f,
+					-0.5f,
+					0.0f,
+					0.0f,
+					+0.5f,
+					0.0f,
+			};
+
+	// Vertex array object
+
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+
+	// vertex buffer object
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+
+	// bind vao
+	glBindVertexArray(VAO);
+	// copy our vertices array in a buffer for OpenGL to use
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// then set our vertex attributes pointers
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	const char *vertexShaderSource = "#version 330 core\n"
+																	 "layout (location = 0) in vec3 aPos;\n"
+																	 "void main()\n"
+																	 "{\n"
+																	 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+																	 "}\0";
+
+	const char *fragmentShaderSource = "#version 330 core\n"
+																		 "out vec4 FragColor;\n"
+																		 "void main()\n"
+																		 "{\n"
+																		 "  FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+																		 "}\0";
+
+	// create vertex shader
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	// create fragment shader
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	// create shader program to use the shaders create
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
 	while (!glfwWindowShouldClose(window))
 	{
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		processInput(window);
+
+		// draw the triangle
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
 		glfwPollEvents();
 		glfwSwapBuffers(window);
 	}
+
+	// delete the shader
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
 
 	glfwTerminate();
 	return 0;

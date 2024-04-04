6e17b45a0cf6b26f507b9a4a8039dc1bb35d82e6
Akanni Emmanuel <akannie33@gmail.com>
2024-04-04 13:23:25 +0100

    perf(renderer): add efficient index buffer usage for improved rendering performance

---
+++ engine/src/renderer/element_buffer.hpp
@@ -0,0 +1,24 @@
+#ifndef HYP_EBO_HPP
+#define HYP_EBO_HPP
+
+#include <glad/glad.h>
+#include <cstdint>
+
+namespace hyp {
+	class ElementBuffer {
+	public:
+		ElementBuffer(uint32_t* indices, uint32_t count);
+
+		void bind();
+		void unbind();
+
+		uint32_t getCount() const { return m_count; }
+	private:
+		uint32_t m_rendererId;
+		uint32_t m_count;
+	};
+
+	using IndexBuffer = ElementBuffer;
+}
+
+#endif // !HYP_EBO_HPP

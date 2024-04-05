2e76db73784a2e2fb02441073ec16f38c79f991e
Akanni Emmanuel <akannie33@gmail.com>
2024-04-05 17:20:19 +0100

    feat(game): made pong using the game engine
    - note: just a prototype pong-game using all the capabilities the engine
    provides

--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -84,13 +84,14 @@ private:
 int main(int argc, char** argv)
 {
 	hyp::Device::init({});
-	hyp::WindowProps props("hyper-sandbox", 600, 600);
+	hyp::WindowProps props("hyper-sandbox: phong", 600, 600);
 	props.resizable = false;
 
 	auto app = hyp::Application(props);
+	hyp::RenderCommand::init();
 	hyp::Renderer2D::init();
 
-	app.pushLayer(new GameLayer({25.f, 125.f}));
+	app.pushLayer(new GameLayer({300.f, 300.f}));
 	app.run();
 
 	hyp::Renderer2D::deinit();

d09ae4d9337f7e70b2428ca13a1faf0697954e45
Akanni Emmanuel <akannie33@gmail.com>
2024-04-06 15:49:50 +0100

    refactor: improve Texture class

--- engine/src/renderer/texture.hpp
+++ engine/src/renderer/texture.hpp
@@ -7,29 +7,81 @@
 #include <string>
 #include <system/export.hpp>
 #include <stb_image.h>
+#include <core/base.hpp>
 
 namespace hyp {
+
+	enum class TextureFormat {
+		None,
+		Red,
+		RGB8,
+		RGBA,
+		RGBA32F,
+	};
+
+	struct TextureSpecification {
+		TextureFormat format = hyp::TextureFormat::RGBA;
+		uint32_t width = 1;
+		uint32_t height = 1;
+		bool mipmap = true;
+	};
+
 	class HYPER_API Texture
 	{
 	public:
-		Texture(const std::string& m_path);
+		Texture(const TextureSpecification& spec);
+		Texture(const std::string& path);
 
-		uint32_t getHeight() const {
-			return m_height;
-		}
+		~Texture();
+
+	public:
+		static hyp::Ref<Texture> create(const TextureSpecification& spec);
+		static hyp::Ref<Texture> create(const std::string& path);
+	public:
+
 		uint32_t getWidth() const {
 			return m_width;
 		}
 
+		uint32_t getHeight() const {
+			return m_height;
+		}
+
+		uint32_t getTextureId() const {
+			return m_texture;
+		}
+
+		bool operator==(const Texture& other) const {
+			return m_texture == other.getTextureId();
+		}
+
 		void bind(int8_t slot = 0);
+
 		void unbind();
+
+		void setData(void* data, uint32_t size);
+		std::string getPath() const{
+			return m_path;
+		}
+
+		bool isLoaded() const {
+			return m_loaded;
+		}
 	private:
+		TextureSpecification m_spec;
+		
 		uint32_t m_texture;
+		uint32_t m_width, m_height;
 		std::string m_path;
-		uint32_t m_width;
-		uint32_t m_height;
-		bool m_loaded;
+		bool m_loaded = false;
+
+		unsigned int m_internalFormat, m_dataFormat;
 	};
+
+	/*
+	* @brief less vague meaning of the Texture class
+	*/
+	using Texture2D = Texture;
 }
 
 #endif

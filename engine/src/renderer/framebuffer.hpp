b4f833b6589a9e1c27d57aa40a1c489de643c2d8
Akanni Emmanuel <akannie33@gmail.com>
2024-04-07 01:36:43 +0100

    feat: implement Framebuffer for off-screen rendering

---
+++ engine/src/renderer/framebuffer.hpp
@@ -0,0 +1,65 @@
+#pragma once
+
+#ifndef HYP_FRAMEBUFFER_HPP
+
+#define HYP_FRAMEBUFFER_HPP
+#include <cstdint>
+#include <core/application.hpp>
+#include <vector>
+
+namespace hyp {
+	enum class FbTextureFormat {
+		None = 0,
+		RGBA,
+	};
+
+	struct FbTextureSpecification {
+		FbTextureSpecification(FbTextureFormat format): textureFormat(format) {
+		}
+
+		FbTextureFormat textureFormat = FbTextureFormat::None;
+	};
+
+	struct FbAttachmentSpecification {
+		FbAttachmentSpecification() = default;
+		FbAttachmentSpecification(std::initializer_list<FbTextureSpecification> attachments)
+			: attachments(attachments) {}
+
+		std::vector<FbTextureSpecification> attachments;
+	};
+
+	struct FramebufferSpecification {
+		uint32_t width, height;
+		FbAttachmentSpecification attachment;
+	};
+	
+	static const int MaxFramebufferSize = 1024 * 8;
+
+	class Framebuffer {
+	public:
+		Framebuffer(const FramebufferSpecification& spec);
+		~Framebuffer();
+
+		static hyp::Ref<Framebuffer> create(const FramebufferSpecification& spec) {
+			return hyp::CreateRef<hyp::Framebuffer>(spec);
+		};
+
+		void bind();
+		void unbind();
+
+		uint32_t getColorAttachmentId(uint32_t index = 0);
+
+		void resize(uint32_t width, uint32_t height);
+
+		void reset();
+	private:
+		uint32_t m_fbo;
+		FramebufferSpecification m_spec;
+
+		std::vector<FbTextureSpecification> m_colorAttachmentSpecs;
+		std::vector<unsigned int> m_colorAttachments;
+	};
+
+}
+
+#endif // !HYP_FRAMEBUFFER_HPP

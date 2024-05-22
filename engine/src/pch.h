49dede6d8ce4e9d9ca7778eddcb54dd07a4c09fe
Akanni Emmanuel <akannie33@gmail.com>
2024-05-22 21:24:00 +0100

    refactor: update dependencies, pre-compile header file

---
+++ engine/src/pch.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#ifdef _WIN32
+	#ifndef NOMINMAX
+		#define NOMINMAX
+	#endif
+#endif
+
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include <unordered_map>
+#include <functional>
+#include <core/base.hpp>
+
+
+#include <utils/assert.hpp>
+#include <utils/logger.hpp>
